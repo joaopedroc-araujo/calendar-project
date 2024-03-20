57e81a2e1ef8c66d3feed96f0a1fc6ebd3267417
joaopedroc-araujo <costaaraujojoaopedro@gmail.com>
2024-03-19 21:18:32 -0300

    Initializing project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import EventCalendar from './components/EventCalendar'
+
+function App() {
+
+  return (
+    <>
+      <EventCalendar />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/EventCalendar/index.tsx
@@ -0,0 +1,11 @@
+const EventCalendar = () => {
+    return (
+        <div className='container mx-auto py-4'>
+            <div>
+                <h2 className='text-center'>Title</h2>
+            </div>
+        </div>
+    );
+};
+
+export default EventCalendar;
